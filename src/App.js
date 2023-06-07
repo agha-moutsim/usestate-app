22ee44d733d9879f935efbff306e2ccab4ad39d1
aghamoutsim <aghamoutsim@gmail.com>
2023-06-07 17:21:03 +0500

    My-counter-App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className='Free'>
       <h1>Counter App</h1>
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
